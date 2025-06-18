c5a1603a44283f7923a25e73fe27faf6dad35a2f
Masih <masihbagheri248@gmail.com>
2025-06-18 07:26:42 +0330

    signup.h was added by b client

--- signup.h
+++ signup.h
@@ -1,37 +1,42 @@
 #ifndef SIGNUP_H
 #define SIGNUP_H
 
+#include <QPushButton>
 #include <QString>
 #include <QWidget>
+#include<QMainWindow>
+#include<Qlabel>
+#include<QMessageBox>
 namespace Ui {
 class SignUp;
 }
 
-class SignUp : public QWidget
+class SignUp : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit SignUp(QWidget *parent = nullptr);
     ~SignUp();
-    bool checkValidFirstName() ;
-    bool checkValidLastName() ;
-    bool checkValidUsername() ;
-    bool checkValidEmail() ;
-    bool checkValidPassword() ;
-    bool checkValidPhoneNumber() ;
+    bool checkValidFirstName();
+    bool checkValidLastName();
+    bool checkValidUsername();
+    bool checkValidEmail();
+    bool checkValidPassword();
+    bool checkValidPhoneNumber();
 
 public slots:
     void onSignupClicked();
 
 private:
-    Ui::SignUp *ui1;
+    Ui::SignUp *ui;
     QString firstName;
     QString lastName;
     QString username;
     QString email;
     QString phoneNumber;
     QString password;
+    QLabel* labelErrorMessage;
 };
 
 #endif // SIGNUP_H
