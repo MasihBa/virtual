98dd93d74a1d87420ea62f1028928666205fc3d5
Masih <masihbagheri248@gmail.com>
2025-07-03 13:05:52 +0330

    main.cpp was adapted for 4 user

--- client/main.cpp
+++ client/main.cpp
@@ -5,7 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    MainWindow w1;
+    MainWindow w2;
+    MainWindow w3;
+    MainWindow w4;
+    w1.show();
+    w2.show();
+    w3.show();
+   w4.show();
     return a.exec();
 }
